6432d2a33358ce4a9d76ed5fcf3f7ebd1e9db259
Rudolf-Gama <gamarudolf2209@gmail.com>
2024-08-16 00:23:29 +0530

    add vite and react basic

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Hey there react using vite</h1>
+    </>
+  )
+}
+
+export default App
---
+++ reactbasic/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hey there,i am learning react</h1>
+  );
+}
+
+export default App;
